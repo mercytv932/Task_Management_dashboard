06a7a4bf26b0f2269e6bc72042052e83991e288d
Yosef <mercytv932@gmail.com>
2026-09-02 21:48:21 -0400

    title useSTate done

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
+
 function TaskForm() {
+  const [title, setTitle] = useState("");
+
+  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const titleChange = event.target.value;
+    setTitle(titleChange);
+  }
   return (
     <form>
       <div>
         <label>Title</label>
-        <input type="text" />
+        <input value={title} onChange={handleTitleChange} type="text" />
         <label>Description</label>
         <textarea></textarea>
       </div>
       <div>
+        <label>Status</label>
         <select>
-          <label>Status</label>
           <option value="pending">Pending</option>
           <option value="in-progress">In-Progress</option>
           <option value="completed">Completed</option>
         </select>
 
+        <label>Priority</label>
         <select>
-          <label>Priority</label>
           <option value="low">Low</option>
           <option value="medium">Medium</option>
           <option value="high">High</option>
